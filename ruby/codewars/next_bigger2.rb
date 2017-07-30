ad67f412d59b9605def32dde0ef91ee40c0887a5
Blogueira <blogueira@blogueiraarteira.com.br>
2017-07-30 19:27:10 -0300

    :(

--- ruby/codewars/next_bigger2.rb
+++ ruby/codewars/next_bigger2.rb
@@ -11,62 +11,78 @@
 # end
 
 def gotohell(n)
-  index = [0,0]
+  
+  # set a few things first
   p "fuk me"
   p "original number: " + n.to_s
-  ary = n.to_s.split('')
-  len = ary.length
+  ar = n.to_s.split('').map(&:to_i)
 
-  (0...len).reverse_each do |i|
-    index[0] = i
-    p i==len-1? "pick a numba " + ary[i].to_s : "pick another numba " + ary[i].to_s
-    no = ary[i]
+  # declare our two indexes
+  i = ar.size
+  o = ar.size
+  limit = 1
+  breakout = false
+  # now what, stuck?
+  
+  ar.size.times do
+    ar.to_enum.with_index.reverse_each do |x, index1|
+  
+      p "index1 = " + ((index1 - ar.size) * -1).to_s
+      p "reality check for first array: " + ar[index1].to_s
+      p "reality check for index1: " + index1.to_s
 
-    # now what
-    p "create subarray :D"
-    sub = ary[0...i]
-    p sub.map(&:to_i)
 
-    # cool! now loop thru it
-    #
-    len2 = sub.length
-    (0...len2).reverse_each do |y|
-      index[1] = y
-      p "indexes: " + index.to_s
-      if ary[i]>sub[y]
-        p ary[i].to_s + " > " + sub[y].to_s
-        prov = ary[i]
-        ary[i] = sub[y]
-        ary[i - sub.size + y] = prov
-        break
-      else
-        p "nope"
+        if breakout
+          breakout = false
+          break
+        end
+    
+      ar[0,index1].to_enum.with_index.reverse_each do |y, index2|
+   
+        # remember this
+        range = (index2 - ar.size) * -1
+        if range > limit
+          limit+=1
+          breakout = true
+        end
+
+        if breakout
+          break
+        end
+        # dont forget that
+      
+        # here's your array
+        # p ar[index1,ar.size][index2]
+
+        # NOW GO FIND SOMEONE TO SWAP YOURSELF WITH
+
+        # p (ar[index1]>ar[index1,ar.size][index2])? "FOUND ITS" : "nope"
+        p "x = " + x.to_s
+        p "y = " + y.to_s
+
+
+
+
+
+
       end
     end
-    p "RESULT!!111"
-    p ary
-    break
+#    p ar[index1]
   end
+    
+
+
+
+
 end
 
-gotohell(29993)
-gotohell(25996)
-gotohell(12)
-gotohell(35)
-gotohell(123)
-gotohell(12565)
+# gotohell(29993)
+# gotohell(25996)
+# gotohell(12)
+# gotohell(35)
+# gotohell(123)
+# gotohell(12565)
 
-gotohell(12324597677765)
+# gotohell(12324597677765)
 
 gotohell(12343)
-
-# Cody Gray
-# Yvette Colomb
-# NathanOliver
-# Floern
-# Andy
-# Alexander O'Mara
-# Baum mit Augen
-# Rob
-# vaultah
-# Travis J

651cb31e9ef75c7a1f84c0119a6dd5bb0a8c17bc
Blogueira <blogueira@blogueiraarteira.com.br>
2017-07-31 02:37:25 -0300

    car

--- ruby/codewars/car_mileage.rb
+++ ruby/codewars/car_mileage.rb
@@ -1,9 +1,11 @@
 def create_palindrome(arr)
-half = (arr.size/2.to_f).ceil
+half = (arr.size/2)
 new_arr = arr
 new_arr[half...arr.size] = arr[0...half].reverse
 arr[half...arr.size] = new_arr[half...new_arr.size]
 arr[0...half] = new_arr[0...half]
+p "generating palindrome:"
+p arr
 return arr
 end
 
@@ -14,11 +16,14 @@ end
 
 def is_interesting(n, ar)
 my_arr = n.to_s.split('').map(&:to_i)
-
-create_palindrome(my_arr)
+p "n = " + n.to_s
+p "palindrome = " + create_palindrome(my_arr).join.to_i.to_s
+if (n - create_palindrome(my_arr).join.to_i) < 3
+  p "holy dog toffee!"
+end
 
 
 end
 
 
-p is_interesting(21654, [])
+p is_interesting(216, [])

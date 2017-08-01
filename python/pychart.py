8087293bf826804bc4a8d0a680206006e569faab
blogueira <blogueira@blogueiraarteira.com.br>
2017-07-31 23:09:19 -0300

    pychart

---
+++ python/pychart.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+labels = 'Test', 'lol', 'fuk'
+sizes = [15, 27, 58]
+plt.pie(sizes, labels=labels, shadow=True)
+plt.show()
---
+++ ruby/codewars/arrivals_departures.rb
@@ -0,0 +1,8 @@
+def flap_display(lines, rotors)
+$ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ?!@#&()|<>.:=-+*/0123456789"
+  p $ALPHABET.split('')
+  p $ALPHABET.split('').index('A')
+end
+
+flap_display(1,2)
+
